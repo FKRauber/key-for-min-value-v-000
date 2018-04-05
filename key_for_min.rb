c8d3f96c51ce8bbf826394381c008527683187bc
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-05 03:35:44 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,9 @@ def key_for_min_value(name_hash)
   low_val = nil
   name_hash.each{ |item, amount|
 
-    if amount = low_val
-
-    else
-
+    if low_val == nil || amount < low_val
+      low_val = amount
+      low_key = item
     end
   }
   low_key
